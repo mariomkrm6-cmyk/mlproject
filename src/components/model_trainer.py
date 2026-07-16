4d9d6a4e3828cfe0c43686211b39ea8297596995
Mario Rashed <mariomkrm6@gmail.com>
2026-07-16 14:40:35 +0300

    api with flask and prediction pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -17,91 +17,101 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object,evaluate_models
+from src.utils import save_object, evaluate_models
+
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join("artifacts","model.pkl")
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
 
 class ModelTrainer:
-    def __init__(self,model_trainer_config:ModelTrainerConfig):
-        self.model_trainer_config=model_trainer_config
-    def initiate_model_trainer(self,train_array,test_array):
+    def __init__(self, model_trainer_config: ModelTrainerConfig):
+        self.model_trainer_config = model_trainer_config
+
+    def initiate_model_trainer(self, train_array, test_array):
         try:
-            logging.info("Split training and test input data")    
-            X_train,X_test,y_train,y_test=(
-                train_array[:,:-1],
-                test_array[:,:-1],
-                train_array[:,-1],
-                test_array[:,-1]
-
-
+            logging.info("Split training and test input data")
+            X_train, X_test, y_train, y_test = (
+                train_array[:, :-1],
+                test_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, -1],
             )
             models = {
-                    "Random Forest": RandomForestRegressor(),
-                    "Decision Tree": DecisionTreeRegressor(),
-                    "Gradient Boosting": GradientBoostingRegressor(),
-                    "Linear Regression": LinearRegression(),
-                    "XGBRegressor": XGBRegressor(),
-                    "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                    "AdaBoost Regressor": AdaBoostRegressor(),
-                }
-            params={
-                    "Decision Tree": {
-                        'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                        # 'splitter':['best','random'],
-                        # 'max_features':['sqrt','log2'],
-                    },
-                    "Random Forest":{
-                        # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    
-                        # 'max_features':['sqrt','log2',None],
-                        'n_estimators': [8,16,32,64,128,256]
-                    },
-                    "Gradient Boosting":{
-                        # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                        'learning_rate':[.1,.01,.05,.001],
-                        'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                        # 'criterion':['squared_error', 'friedman_mse'],
-                        # 'max_features':['auto','sqrt','log2'],
-                        'n_estimators': [8,16,32,64,128,256]
-                    },
-                    "Linear Regression":{},
-                    "XGBRegressor":{
-                        'learning_rate':[.1,.01,.05,.001],
-                        'n_estimators': [8,16,32,64,128,256]
-                    },
-                    "CatBoosting Regressor":{
-                        'depth': [6,8,10],
-                        'learning_rate': [0.01, 0.05, 0.1],
-                        'iterations': [30, 50, 100]
-                    },
-                    "AdaBoost Regressor":{
-                        'learning_rate':[.1,.01,0.5,.001],
-                        # 'loss':['linear','square','exponential'],
-                        'n_estimators': [8,16,32,64,128,256]
-                    }
-                    
-                }
-            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, param=params)
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+            params = {
+                "Decision Tree": {
+                    "criterion": [
+                        "squared_error",
+                        "friedman_mse",
+                        "absolute_error",
+                        "poisson",
+                    ],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest": {
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2',None],
+                    "n_estimators": [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "CatBoosting Regressor": {
+                    "depth": [6, 8, 10],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "iterations": [30, 50, 100],
+                },
+                "AdaBoost Regressor": {
+                    "learning_rate": [0.1, 0.01, 0.5, 0.001],
+                    # 'loss':['linear','square','exponential'],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+            }
+            model_report: dict = evaluate_models(
+                X_train=X_train,
+                y_train=y_train,
+                X_test=X_test,
+                y_test=y_test,
+                models=models,
+                param=params,
+            )
 
-        # This does the exact same thing as all that crazy bracket code!
+            # This does the exact same thing as all that crazy bracket code!
             best_model_name = max(model_report, key=model_report.get)
-            best_model_score=model_report[best_model_name]
-            best_model=models[best_model_name]
+            best_model_score = model_report[best_model_name]
+            best_model = models[best_model_name]
 
-            if best_model_score<0.6:
+            if best_model_score < 0.6:
                 raise CustomException("no best model found")
             logging.info("Best found model on both training and testing dataset")
-
-            save_object(
-                    file_path=self.model_trainer_config.trained_model_file_path,
-                    obj=best_model
-                )
             best_model.fit(X_train, y_train)
-            predicted=best_model.predict(X_test)
-            r2_square=r2_score(y_test,predicted)
-            return  r2_square
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model,
+            )
+
+            predicted = best_model.predict(X_test)
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
         except Exception as e:
-            raise CustomException(e,sys)
-           
+            raise CustomException(e, sys)
